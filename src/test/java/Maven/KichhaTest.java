51e1204df380f87de5500f7abd4467c3e0bf2c2b
User <User@User-PC>
2022-03-14 23:38:35 +0530

    FilmActore and Their films

---
+++ src/test/java/Maven/KichhaTest.java
@@ -0,0 +1,25 @@
+package Maven;
+
+import org.testng.annotations.Test;
+
+public class KichhaTest
+{
+@Test
+public void kichhaTest()
+{
+	System.out.println("suudeep best actor in kannada industry");
+	System.out.println("nalla is the best movie");
+}
+@Test
+public void HatricTest()
+{
+	System.out.println("shivaraj best actor in kannada industry");
+	System.out.println("jogi is the best film");
+}
+@Test
+public void SahasaSihmaTest()
+{
+	System.out.println("vishnu sir best actor in kannada industry");
+	System.out.println(" ramachari is the best movie");
+}
+}
